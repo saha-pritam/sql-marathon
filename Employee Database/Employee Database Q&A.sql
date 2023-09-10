536d580453becc8b2afe89138ff1ca34b4bb7f47
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-10 18:36:02 +0530

    Added extra 14 curated questions for employee database and also their answers. All total now there are 61 questions.

--- Employee Database/Employee Database Q&A.sql
+++ Employee Database/Employee Database Q&A.sql
@@ -141,6 +141,48 @@ select * from employees where hire_date>='1991-07-01' and hire_date<='1991-12-31
 -- 47. Write a SQL query to find those employees who joined in 90's. Return complete information about the employees. 
 select * from employees where date_format(hire_date,'%y')='90';
 
+-- 48. Write a SQL query to find those managers who are in the department 1001 or 2001. Return complete information about the employees. 
+select * from employees where job_name='MANAGER' and dep_id in (1001,2001);
+
+-- 49. Write a SQL query to identify employees who joined in the month of FEBRUARY with a salary range of 1001 to 2000 (Begin and end values are included.). Return complete information about the employees. 
+select * from employees where date_format(hire_date,'%M')='FEBRUARY' and salary between 1001 and 2000;
+
+-- 50. Write a SQL query to find those employees who joined before or after the year 1991. Return complete information about the employees. 
+select * from employees where hire_date<'1991-01-01' or hire_date>'1991-12-31';
+
+-- 51. Write a SQL query to find employees along with their department details. Return employee ID, employee name, job name, manager ID, hire date, salary, commission, department ID, and department name.
+select emp_id, emp_name, job_name, manager_id, hire_date, salary, commission, dep_id, dep_name from employees natural join department;
+
+-- 52. Write a SQL query to identify those employees who earn 60000 or more per year or do not work as ANALYST. Return employee name, job name, (12*salary) as Annual Salary, department ID, and grade. 
+select emp_name, job_name, 12*salary 'Annual Salary', dep_id, grade from employees join salary_grade on (12*salary>60000 or job_name<>'ANALYST') and salary>=min_salary and salary<=max_salary;
+
+-- 53. Write a SQL query to identify employees whose salaries are higher than their managers' salaries. Return employee name, job name, manager ID, salary, manager name, manager's salary. 
+ select e1.emp_name, e1.job_name, e1.manager_id, e1.salary, e2.emp_name as manager_name, e2.salary as managers_salary from employees e1 join employees e2 on e1.manager_id=e2.emp_id and e1.salary>e2.salary;
+ 
+ -- 54. Write a SQL query to find those employees whose salary is between 2000 and 5000 (Begin and end values are included.) and location is PERTH. Return employee name, department ID, salary, and commission. 
+ select emp_name, employees.dep_id, salary, commission from employees join department on salary between 2000 and 5000 and dep_location='PERTH' and employees.dep_id=department.dep_id;
+ 
+ -- 55. Write a SQL query to find the employees whose department ID is 1001 or 3001 and whose salary grade is not 4. They joined the company before 1992-12-31. Return grade, employee name. 
+ select emp_name, grade from employees join salary_grade on dep_id in (1001,3001) and hire_date<'1992-12-31' and salary>=min_salary and salary<=max_salary and grade<>4;
+ 
+ -- 56. Write a SQL query to find those employees whose manager name is JONAS. Return employee id, employee name, job name, manager ID, hire date, salary, department ID, manager name. 
+ select e1.emp_id, e1.emp_name, e1.job_name, e1.manager_id, e1.hire_date, e1.dep_id, e2.emp_name as manager_name from employees e1 join employees e2 on e2.emp_name='JONAS' and e1.manager_id=e2.emp_id;
+ 
+ -- 57. Write a SQL query to find the name and salary of the employee FRANK.
+ select emp_name, salary from employees where emp_name='FRANK';
+ 
+ -- 58. Write a SQL query to search for employees who are working either as a MANAGER or an ANALYST with a salary between 2000 and 5000 (Begin and end values are included.) without commissions (null). Return complete information about the employees 
+ select * from employees where job_name in ('MANAGER','ANALYST') and salary between 2000 and 5000 and commission is null;
+ 
+ -- 59. Write a SQL query to search for employees working in PERTH or MELBOURNE and their achieved experience is more than 10 years. Return employee ID, employee name, department ID, salary, and department location.
+select emp_id, emp_name, dep_ID, salary, dep_location from employees natural join department where dep_location in ('PERTH','MELBOURNE') and timestampdiff(YEAR,hire_date,curdate())>10;
+
+-- 60. Write a SQL query to find the employees who joined in 1991 and whose department location is SYDNEY or MELBOURNE with a salary range of 2000 to 5000 (Begin and end values are included.). Return employee ID, employee name, department ID, salary, and department location.
+select emp_ID, emp_name, employees.dep_ID, salary, dep_location from employees join department on date_format(hire_date,'%Y')='1991' and salary between 2000 and 5000 and dep_location in ('SYDNEY','MELBOURNE') and department.dep_id=employees.dep_id; 
+
+-- 61. Write a SQL query to find the employees of MARKETING department come from MELBOURNE or PERTH, are in grades 3 ,4, and 5 and have at least 25 years of experience. Return department ID, employee ID, employee name, salary, department name, department location and grade.
+select d.dep_ID, e.emp_ID, e.emp_name, e.salary, d.dep_name, d.dep_location, g.grade from employees e join department d on timestampdiff(YEAR,hire_date,curdate())>25 and d.dep_name='MARKETING' and d.dep_location in ('MELBOURNE','PERTH') and e.dep_id=d.dep_id join salary_grade g on e.salary>=g.min_salary and e.salary<=g.max_salary and g.grade in (3,4,5);
+
 
 
 
