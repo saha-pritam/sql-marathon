1ed0e44598c1169e5a42f00e12860b604beb2bcc
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-14 13:59:49 +0530

    Added extra 8 curated questions for inventory database and also their answers. All total now there are 70 questions.

--- Inventory Database/Inventory Database Q&A.sql
+++ Inventory Database/Inventory Database Q&A.sql
@@ -183,3 +183,29 @@ select customer_id, count(ord_no) as 'number of distinct orders', max(purch_amt)
 
 -- 62. Write a SQL query to calculate the summation of purchase amount, total commission (15% for all salespeople) by each order date. Sort the result-set on order date. Return order date, summation of purchase amount and commission.
 select ord_date, sum(purch_amt) as 'summation of purchase amount',  sum(purch_amt)*0.15 as 'Commission' from orders group by ord_date order by ord_date;
+
+-- 63. Write a SQL query to find the salespeople and customers who live in the same city. Return customer name, salesperson name and salesperson city. 
+select c.cust_name, s.name, c.city from salesman s join customer c using(city);
+
+-- 64. Write a SQL query to locate all the customers and the salesperson who works for them. Return customer name, and salesperson name. 
+select cust_name, name from salesman join customer using(salesman_id);
+
+-- 65. Write a SQL query to find those salespeople who generated orders for their customers but are not located in the same city. Return ord_no, cust_name, customer_id (orders table), salesman_id (orders table). 
+select o.ord_no, c.cust_name, o.customer_id, o.salesman_id from salesman s join orders o on s.salesman_id=o.salesman_id join customer c on c.customer_id=o.customer_id and s.city<>c.city; 
+
+-- 66. Write a SQL query to locate the orders made by customers. Return order number, customer name. 
+select ord_no, cust_name from orders natural join customer;
+
+-- 67. Write a SQL query to find those customers where each customer has a grade and is served by a salesperson who belongs to a city. Return cust_name as "Customer", grade as "Grade". 
+select cust_name as 'Customer', grade as 'Grade' from customer join salesman using(salesman_id) where grade is not null and salesman.city is not null;
+
+-- 68. Write a SQL query to find those customers who are served by a salesperson and the salesperson earns commission in the range of 12% to 14% (Begin and end values are included.). Return cust_name AS "Customer", city AS "City".  
+select cust_name AS "Customer", customer.city AS "City" from salesman join customer using(salesman_id) where commission between 0.12 and 0.14;
+
+-- 69. Write a SQL query to find all orders executed by the salesperson and ordered by the customer whose grade is greater than or equal to 200. Compute purch_amt*commission as “Commission Amount”. Return order no, customer name, commission and Commission amount.  
+select ord_no, cust_name, commission , purch_amt * commission as 'Commission Amount' from orders join customer on orders.customer_id=customer.customer_id and customer.grade>=200 join salesman on salesman.salesman_id=orders.salesman_id; 
+
+-- 70. Write a SQL query to find those customers who placed orders on October 5, 2012. Return customer_id, cust_name, city, grade, salesman_id, ord_no, purch_amt, ord_date, customer_id and salesman_id.
+select c.customer_id, c.cust_name, c.city, c.grade, o.salesman_id, o.ord_no, o.purch_amt, o.ord_date, o.customer_id, o.salesman_id from customer c natural join orders o where o.ord_date='2012-10-05';
+
+ 
